abbff5d9ea72b30920f562183294557359da8276
Hadrien Levionnois <hlevionnois@hellowork.com>
2025-01-14 12:19:04 +0100

    Simplify action

--- src/Action/AddShameAction.php
+++ src/Action/AddShameAction.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace App\Action;
 
 use App\Domain\Shame\AddShame;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[AsController]
 #[Route('/shame', methods: ["POST"])]
-class AddShameAction extends AbstractController
+class AddShameAction
 {
-    public function __construct(private readonly AddShame $handler) {}
-
-    public function __invoke(#[MapRequestPayload] AddShamePayload $shamePayload): JsonResponse
-    {
-        return new JsonResponse($this->handler->add($shamePayload), 201);
+    public function __invoke(
+        AddShame $handler,
+        #[MapRequestPayload] AddShamePayload $shamePayload
+    ): JsonResponse {
+        return new JsonResponse($handler->add($shamePayload), 201);
     }
 }
---
+++ tests/Action/AddShameActionTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Tests\Action;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AddShameActionTest extends WebTestCase
+{
+
+}
